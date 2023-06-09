639b4eda807119f8918211a0df7546df1d1b63c9
Oleksandr Siukov <as-service2008@ukr.net>
2023-06-09 11:28:13 +0300

    add

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,37 @@
+// Импортируем хук
+import { useSelector, useDispatch } from 'react-redux';
+// Импортируем генератор экшена
+import { setStatusFilter } from '../../redux/actions';
+// Импортируем объект значений фильтра
+import { statusFilters } from '../../redux/constants';
+import { Button } from 'components/Button/Button';
+export const StatusFilter = () => {
+  // Получаем ссылку на функцию отправки экшенов
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.statusFilter);
+  // Вызываем генератор экшена и передаём значение фильтра
+  // Отправляем результат - экшен изменения фильтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+  return (
+    <div>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
